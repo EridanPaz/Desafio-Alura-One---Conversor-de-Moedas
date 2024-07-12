a9c42ba88c26cedbc7691b0b8f9407669dae67e6
Eridan Paz <eribpaz@gmail.com>
2024-07-12 19:22:57 -0300

    [feat]: Commit inicial.

---
+++ src/main/java/com/alura/conversor_de_moeda/ConversorDeMoedaApplication.java
@@ -0,0 +1,13 @@
+package com.alura.conversor_de_moeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeMoedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/conversor_de_moeda/ConversorDeMoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.conversor_de_moeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeMoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
